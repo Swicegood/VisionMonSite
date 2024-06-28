8d9dbc65f89a4b4dc8000e84a7b9392614862a6b
Jagadguru <jguru108@gmail.com>
2024-06-28 10:38:23 -0400

    route favicon

--- vision_monitor_website/monitor/urls.py
+++ vision_monitor_website/monitor/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('monitor/', views.monitor, name='monitor'),
     path('test_websocket/', views.test_websocket, name='test_websocket'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
