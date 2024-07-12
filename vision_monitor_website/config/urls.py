7b881faf965b5ed7f8b41d6a2239dea916d19e2b
Jagadguru <jguru108@gmail.com>
2024-07-12 16:32:11 -0400

    attempt to add image feed

--- vision_monitor_website/config/urls.py
+++ vision_monitor_website/config/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+from . import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+       path('admin/', admin.site.urls),
     path('', include('monitor.urls')),
+    path('get_latest_image/<int:camera_index>/', views.get_latest_image, name='get_latest_image'),
 ]
