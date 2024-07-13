03e80c9f7d05c8b2c622aedf4c0b4981d4cec22f
Jagadguru <jguru108@gmail.com>
2024-07-13 14:51:41 -0400

    attempt to fix aioredis incomat

--- vision_monitor_website/monitor/consumers.py
+++ vision_monitor_website/monitor/consumers.py
@@ -2,13 +2,12 @@ import json
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from redis.asyncio import Redis
 import os
-import aioredis
-from django.conf import settings
 
-# Redis configuration
 REDIS_HOST = os.getenv('REDIS_HOST', '192.168.0.71')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_MESSAGE_CHANNEL = 'llm_messages'
 REDIS_STATE_CHANNEL = 'state_processing'
 REDIS_STATE_RESULT_CHANNEL = 'state_result'
 
@@ -16,13 +15,12 @@ class LLMOutputConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.channel_layer.group_add("llm_output", self.channel_name)
         await self.accept()
-        self.redis = await aioredis.create_redis_pool(f'redis://{REDIS_HOST}:{REDIS_PORT}')
-        asyncio.create_task(self.listen_for_state_results())
+        self.redis = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+        asyncio.create_task(self.listen_for_redis_messages())
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("llm_output", self.channel_name)
-        self.redis.close()
-        await self.redis.wait_closed()
+        await self.redis.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -45,23 +43,14 @@ class LLMOutputConsumer(AsyncWebsocketConsumer):
     async def request_state_processing(self):
         await self.redis.rpush(REDIS_STATE_CHANNEL, "process")
 
-    async def listen_for_state_results(self):
-        channel = self.redis.pubsub()
-        await channel.subscribe(REDIS_STATE_RESULT_CHANNEL)
+    async def listen_for_redis_messages(self):
+        pubsub = self.redis.pubsub()
+        await pubsub.subscribe(REDIS_MESSAGE_CHANNEL, REDIS_STATE_RESULT_CHANNEL)
         try:
-            async for message in channel.listen():
+            async for message in pubsub.listen():
                 if message['type'] == 'message':
-                    state_data = json.loads(message['data'])
-                    await self.send(text_data=json.dumps(state_data))
+                    await self.send(text_data=message['data'])
         except Exception as e:
-            print(f"Error listening for state results: {str(e)}")
+            print(f"Error listening for Redis messages: {str(e)}")
         finally:
-            await channel.unsubscribe(REDIS_STATE_RESULT_CHANNEL)
-
-    async def send_states(self, event):
-        facility_state = event['facility_state']
-        camera_states = event['camera_states']
-        await self.send(text_data=json.dumps({
-            'facility_state': facility_state,
-            'camera_states': camera_states
-        }))
+            await pubsub.unsubscribe(REDIS_MESSAGE_CHANNEL, REDIS_STATE_RESULT_CHANNEL)
