bf636906cae8d71e7015a1f983195b844c92fc2a
Jagadguru <jguru108@gmail.com>
2024-07-15 14:52:25 -0400

    set ddaphne executable path

--- start-server.sh
+++ start-server.sh
@@ -3,6 +3,9 @@ set -e
 
 # We're already in /opt/app/vision_monitor_website due to the WORKDIR in Dockerfile
 
+# Set default path for Daphne if not specified
+DAPHNE_EXECUTABLE_PATH=${DAPHNE_EXECUTABLE_PATH:-"~/.vision_mon_site/bin/"}
+
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate --noinput
@@ -13,13 +16,12 @@ python manage.py collectstatic --noinput
 
 # Start Daphne for ASGI support
 echo "Starting Daphne..."
-~/.vision_mon_site/bin/daphne -b 0.0.0.0 -p 8001 config.asgi:application &
+${DAPHNE_EXECUTABLE_PATH}daphne -b 0.0.0.0 -p 8001 config.asgi:application &
 
+# Start Redis Listener
 echo "Starting Redis Listener..."
 python manage.py redis_listener --daemon
 
 # Start Nginx
 echo "Starting Nginx..."
 nginx -g "daemon off;"
-
-
