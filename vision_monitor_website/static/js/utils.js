b4442d2d1b282f94b73401cfc0f7a3f0d8258cf5
Jagadguru <jguru108@gmail.com>
2025-01-05 18:37:36 -0500

    refactor: update state detection logic for gathering events
    - Modified the state detection logic in `utils.js` to include 'gathering' in addition to 'religious' for better categorization of events.
    - This change enhances the clarity of state representation in the UI by broadening the conditions under which the 'bg-warning' class is applied.
    
    These updates improve the maintainability and accuracy of the state detection logic in the application.

--- vision_monitor_website/static/js/utils.js
+++ vision_monitor_website/static/js/utils.js
@@ -15,7 +15,7 @@ export function colorCodeState(element, state) {
         element.classList.add('bg-danger', 'text-white');
     } else if (lowerState.includes('night-time')) {
         element.classList.add('bg-secondary', 'text-white');
-    } else if (lowerState.includes('religious') || lowerState.includes('religious') 
+    } else if (lowerState.includes('religious') || lowerState.includes('gathering') 
         || lowerState.includes('eating') ) {
         element.classList.add('bg-warning');
     } else if (lowerState.includes('nothing')) {
